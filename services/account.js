e2c8d71888cd4826fdf03a53875d3e9dcc303648
Cory Virok <cory@rollbar.com>
2020-12-31 13:36:45 -0800

    Add comment to schema explaining the situation

--- services/account.js
+++ services/account.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
   }
 
   extend type AccountSettings @key(fields: "account { id }") {
+    # This @external causes the GraphQL parser to halt with
+    # https://github.com/apollographql/apollo-server/issues/4378
     account: Account! @external
     relatedAccounts: [Account]!
   }
